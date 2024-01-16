e1065747bbf78d15e899d595b5d7af899d00eaee
Felipe Rodrigues <133248921+FelipeDev00@users.noreply.github.com>
2024-01-16 14:28:03 -0300

    Atividade 1.sql

--- Atividade 1/Atividade 1.sql
+++ Atividade 1/Atividade 1.sql
@@ -1,16 +1,14 @@
-USE uc4atividades;
-
 CREATE USER 'user_relatorio'@'localhost' IDENTIFIED BY '';
-
-SELECT * FROM mysql.user;
 
 CREATE USER 'user_funcionario'@'localhost' IDENTIFIED BY '';
 
+SELECT * FROM mysql.user;
+
 CREATE ROLE 'relatorio'; 
 
-GRANT SELECT ON uc4atividades.* TO 'relatorio';
-
 CREATE ROLE 'funcionario';
+
+GRANT SELECT ON uc4atividades.* TO 'relatorio';
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.venda TO 'funcionario';
 
@@ -18,8 +16,12 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.cliente TO 'funcionario';
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.produto TO 'funcionario';
 
-GRANT relatorio TO 'user_relatorio'@'localhost';
+GRANT 'relatorio' TO 'user_relatorio'@'localhost';
 
-GRANT funcionario TO 'user_funcionario'@'localhost';
+GRANT 'funcionario' TO 'user_funcionario'@'localhost';
 
 FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'user_relatorio'@'localhost' USING 'relatorio';
+
+SHOW GRANTS FOR 'user_funcionario'@'localhost' USING 'funcionario';
